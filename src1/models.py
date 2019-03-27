71d98c382be9e2f028e0bc66608abc3a9c2a1017
gcpportal <48978176+gcpportal@users.noreply.github.com>
2019-03-27 12:55:12 +0530

    Update models.py

--- src1/models.py
+++ src1/models.py
@@ -4,7 +4,7 @@ Created on Jan 22, 2019
 @author: deepashree.kurnad
 '''
 
-from google.appengine.ext import ndb
+import ndb
 
 class GCPRequest(ndb.Model):
     applicationName = ndb.StringProperty()
